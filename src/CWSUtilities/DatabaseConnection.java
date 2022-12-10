9a1e1e1973f0b842a4c921a837da6ef3c1032ee4
anirudhajoshi2808 <joshi.anir@northeastern.edu>
2022-12-09 20:04:02 -0500

    Database Connection Changes

--- src/CWSUtilities/DatabaseConnection.java
+++ src/CWSUtilities/DatabaseConnection.java
@@ -66,7 +66,7 @@ public class DatabaseConnection {
             setConnection();
             PreparedStatement ps;
 
-            ps = connection.prepareStatement("INSERT INTO extra_person VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+            ps = connection.prepareStatement("INSERT INTO Person_Child_Welfare_Sys VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,?)", Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, person.getFirstName());
             ps.setString(2, person.getLastName());
             ps.setString(3, person.getEmail());
@@ -75,8 +75,10 @@ public class DatabaseConnection {
             ps.setString(6, person.getCity());
             ps.setInt(7, person.getZipCode());
             ps.setString(8, person.getState());
-            ps.setString(9, person.getUsername());
-            ps.setString(10, person.getPassword());
+            ps.setString(9, person.getDate());
+            ps.setString(10, person.getUsername());
+            ps.setString(11, person.getPassword());
+            ps.setString(12, person.getRole());
             
             ps.executeUpdate();
             resultSet = ps.getGeneratedKeys();
@@ -162,7 +164,65 @@ public class DatabaseConnection {
         return resultSet;
     
     }
-  
+    
+    public static ResultSet getPersonCWCRole(String username, boolean isDml) throws SQLException {
+        connectDB();
+        String query = "SELECT password,role FROM Person_Child_Welfare_Sys WHERE username = "+"\'"+username+"\'";
+        ResultSet resultSet = null;
+        if (isDml) {
+            statement.executeUpdate(query);
+            return null;
+        }
+
+        resultSet = statement.executeQuery(query);
+
+        return resultSet;
+    }
+    
+    public static ResultSet updatePersonCWC(PersonCWC person){
+        
+        ResultSet resultSet = null;
+        try {
+            setConnection();
+            PreparedStatement ps;
+
+            ps = connection.prepareStatement("Update Person_Child_Welfare_Sys SET firstname =?, lastname =?, mobile =  ?, address = ?, city = ?, zipcode = ?, state = ?, password = ? where username = ?", Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setString(1, person.getFirstName());
+            ps.setString(2, person.getLastName());
+            ps.setLong(3, person.getMobile());
+            ps.setString(4, person.getAddress());
+            ps.setString(5, person.getCity());
+            ps.setInt(6, person.getZipCode());
+            ps.setString(7, person.getState());
+            ps.setString(8, person.getPassword());
+            ps.setString(9, person.getUsername());
+
+            
+            ps.executeUpdate();
+            resultSet = ps.getGeneratedKeys();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return resultSet;
+    
+    }
+    
+    public static ResultSet getDeletePersonCWC(String username, boolean isDml) throws SQLException {
+        connectDB();
+        String query = Constants.CWCentrePersonDelete+"\'"+username+"\'";
+        ResultSet resultSet = null;
+        if (isDml) {
+            statement.executeUpdate(query);
+            return null;
+        }
+
+        resultSet = statement.executeQuery(query);
+
+        return resultSet;
+    }
 }
 
 
